6e2ff39cd1c6b5bb86c535635d1121872803518d
Johan Van de Wauw <johan@fluves.com>
2020-10-23 22:08:32 +0200

    Don't make pytest skip our code

--- testfiles/test_compare_output.py
+++ testfiles/test_compare_output.py
@@ -9,9 +9,10 @@ from test_benchmark import _compare_folder, _get_filenames, _compare_rst_folder
 
 # check 'modeloutput' files
 
-refoutput = Path("testfiles/molenbeek/modeloutput_ref")
-output = Path("testfiles/molenbeek/modeloutput")
+def test_all():
+    refoutput = Path("testfiles/molenbeek/modeloutput_ref")
+    output = Path("testfiles/molenbeek/modeloutput")
 
-assert _get_filenames(refoutput) == _get_filenames(output)
+    assert _get_filenames(refoutput) == _get_filenames(output)
 
-_compare_rst_folder(refoutput, output)
+    _compare_rst_folder(refoutput, output)
