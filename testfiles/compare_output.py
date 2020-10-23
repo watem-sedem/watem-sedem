53d97db145fbde8687ef56f6387c254ef99f12cb
Johan Van de Wauw <johan@fluves.com>
2020-10-23 16:49:15 +0200

    Add initial python tests
    Note this is *very* dirty: test_benchmark contains a slightly modified
    copy of https://git.fluves.net/fluves/CNWS-processing-scripts/src/branch/master/cnws/tests/test_benchmark.py
    
    committing anyway as I'd like to focus first on getting these tests
    running

---
+++ testfiles/compare_output.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from test_benchmark import _compare_folder, _get_filenames, _compare_rst_folder
+
+
+# not feasible as we don't have subdirs
+#_compare_folder(Path('testfiles/molenbeek/modeloutput_ref'), Path('testfiles/molenbeek/modeloutput'))
+
+
+# check 'modeloutput' files
+
+refoutput = Path("testfiles/molenbeek/modeloutput_ref")
+output = Path("testfiles/molenbeek/modeloutput")
+
+assert _get_filenames(refoutput) == _get_filenames(output)
+
+_compare_rst_folder(refoutput, output)
---
+++ testfiles/test_benchmark.py
@@ -0,0 +1,165 @@
+import pytest
+
+import os
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+
+from cnws.utils import load_raster, load_total_sediment_file
+
+
+def _get_filenames(path):
+    """"""
+    return set(
+        [item.name for item in path.iterdir() if ".rst.aux.xml" not in item.name]
+    )
+
+def equal_rst(rst_file_1, rst_file_2, rtol=1e-8, atol=1e-8):
+    """Check if idrisi files are the same"""
+    s1, s2 = load_raster(rst_file_1), load_raster(rst_file_2)
+    assert np.allclose(s1[0], s2[0], rtol=rtol, atol=atol, equal_nan=True), \
+            "file %s and %s differ" % (rst_file_1, rst_file_2)
+
+def _compare_folder(
+    folder_benchmark,
+    folder_output,
+    year="2018",
+    name="molenbeek",
+    scenario="scenario_1",
+):
+    """Compare the setup of the folder/file structure
+
+    Notes
+    -----
+    The filecmp, https://docs.python.org/3/library/filecmp.html, is not used as the file
+    content can differ with a given precision on value-level. This functions checks
+    which files/folders, specific functions are defined to test the data content.
+    """
+    # Check main folder is user defined name
+    assert _get_subdir_names(folder_benchmark) == _get_subdir_names(folder_output)
+    assert _get_subdir_names(folder_output) == {name}
+
+    # Check data and catchment subfolders
+    assert _get_subdir_names(folder_benchmark / name).issuperset(
+        _get_subdir_names(folder_output / name)
+    )
+    assert {"Data_Bekken", scenario}.issubset(_get_subdir_names(folder_output / name))
+
+    # Check scenario subfolders
+    assert _get_subdir_names(folder_output / name / scenario) == {
+        year,
+        "modelinput",
+        "modeloutput",
+        "postprocessing",
+    }
+
+    # check 'modelinput' files
+    assert _get_filenames(
+        folder_benchmark / name / scenario / "modelinput"
+    ) == _get_filenames(folder_output / name / scenario / "modelinput")
+
+    # check 'modeloutput' files
+    assert _get_filenames(
+        folder_benchmark / name / scenario / "modelinput"
+    ) == _get_filenames(folder_output / name / scenario / "modelinput")
+
+    # check 'Data_bekken' files
+    assert _get_filenames(folder_benchmark / name / "Data_Bekken") == _get_filenames(
+        folder_output / name / "Data_Bekken"
+    )
+
+    # check 'year' files
+    assert _get_filenames(folder_benchmark / name / scenario / year) == _get_filenames(
+        folder_output / name / scenario / year
+    )
+
+
+def _compare_rst_folder(ref, new):
+    """ Compare RST files in two folders
+    """
+    for file_name in ref.glob("*.rst"):
+
+        # adjust the numerical tolerance as function of the data; cumulative
+        # data need to have more relative tolerance
+        # TODO sgobeyn/daanr - revise these categories
+        if file_name.name in [
+            "WATEREROS (mm per gridcel).rst",
+            "RUSLE.rst",
+            "Capacity.rst",
+            "SediExport_kg.rst",
+            "SediIn_kg.rst",
+            "SLOPE.rst",
+        ]:
+            rtol, atol = 1e-5, 1e-3
+        elif file_name.name in ["WATEREROS (kg per gridcel).rst"]:
+            rtol, atol = 1e-4, 1e-6
+        elif file_name.name in [
+            "LS.rst",
+            "AspectMap.rst",
+            "sewer_in.rst",
+            "cumulative.rst",
+            "UPAREA.rst",
+            "SediOut_kg.rst",
+        ]:
+            rtol, atol = 1e-5, 1e-8
+        else:
+            rtol, atol = 1e-8, 1e-8
+        equal_rst(
+            file_name,
+            (new / file_name.name),
+            rtol=rtol,
+            atol=atol,
+        )
+
+
+def _compare_rst(
+    folder_benchmark,
+    folder_output,
+    year="2018",
+    name="molenbeek",
+    scenario="scenario_1",
+):
+    """Compare RST files for a scenario"""
+    """rst files in input and outfolder should be the same"""
+    for folder in ['modelinput', 'modeloutput']:
+        _compare_rst_folder( folder_benchmark / name / scenario / folder,
+                folder_output / name / scenario / folder)
+
+
+def _compare_table(
+    folder_benchmark,
+    folder_output,
+    year="2018",
+    name="molenbeek",
+    scenario="scenario_1",
+):
+    """rst files in input and outfolder should be the same"""
+    for folder in ["modelinput", "modeloutput"]:
+        for file_name in (folder_benchmark / name / scenario / folder).glob("*.txt"):
+            if file_name.name == "Total sediment.txt":
+                equal_total_sediment(
+                    file_name,
+                    (folder_output / name / scenario / folder / file_name.name),
+                    rtol=1e-8,
+                    atol=1e-8,
+                )
+            elif file_name.name == "Total sediment VHA.txt":
+                equal_table(
+                    file_name,
+                    (folder_output / name / scenario / folder / file_name.name),
+                    rtol=1e-8,
+                    atol=1e-8,
+                    skiplines=1,
+                )
+            else:
+                equal_table(
+                    file_name,
+                    (folder_output / name / scenario / folder / file_name.name),
+                    rtol=1e-8,
+                    atol=1e-8,
+                    skiplines=0,
+                )
+
+
