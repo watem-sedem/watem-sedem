c40abb616bb368aa8a181f7d3a9ca9a156d34d87
Johan Van de Wauw <johan@fluves.com>
2020-10-27 08:23:08 +0100

    Also check modelinput_ref

--- testfiles/test_compare_output.py
+++ testfiles/test_compare_output.py
@@ -9,10 +9,23 @@ from test_benchmark import _compare_folder, _get_filenames, _compare_rst_folder
 
 # check 'modeloutput' files
 
-def test_all():
+def test_modeloutput():
     refoutput = Path("testfiles/molenbeek/modeloutput_ref")
     output = Path("testfiles/molenbeek/modeloutput")
 
     assert _get_filenames(refoutput) == _get_filenames(output)
+    _compare_rst_folder(refoutput, output)
 
+
+def test_modelinput_ref():
+    """This tests the file under modelinput_ref
+
+    The two files under modelinput_ref are two files
+    that are actually being written by the model,
+    so these should be checked as well.
+    """
+    refoutput = Path("testfiles/molenbeek/modelinput_ref")
+    output = Path("testfiles/molenbeek/modelinput")
+
+    assert _get_filenames(refoutput).issubset(_get_filenames(output))
     _compare_rst_folder(refoutput, output)
