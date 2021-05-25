d7827a5069e1b41b00f3d92ba7817a7847e4fb4d
Johan Van de Wauw <johan@fluves.com>
2021-05-25 13:01:20 +0200

    don't complain if file does not exist

--- testfiles/test.sh
+++ testfiles/test.sh
@@ -1,5 +1,5 @@
 set -e
-rm -r testfiles/molenbeek/modeloutput
-rm -r testfiles/molenbeek/modeloutput_sdat
+rm -rf testfiles/molenbeek/modeloutput
+rm -rf testfiles/molenbeek/modeloutput_sdat
 cn_ws/cn_ws testfiles/molenbeek/modelinput_sdat/ini_molenbeek_scenario_1_sdat.ini
 cn_ws/cn_ws testfiles/molenbeek/modelinput/ini_molenbeek_scenario_1.ini
