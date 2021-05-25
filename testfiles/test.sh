ec197c4950711564f3ede64b54204fd24bd9e5ff
Johan Van de Wauw <johan@fluves.com>
2021-05-25 10:36:45 +0200

    Make sure script stops if one step fails

--- testfiles/test.sh
+++ testfiles/test.sh
@@ -1,2 +1,3 @@
-cn_ws/cn_ws testfiles/molenbeek/modelinput/ini_molenbeek_scenario_1.ini
+set -e
 cn_ws/cn_ws testfiles/molenbeek/modelinput_sdat/ini_molenbeek_scenario_1_sdat.ini
+cn_ws/cn_ws testfiles/molenbeek/modelinput/ini_molenbeek_scenario_1.ini
