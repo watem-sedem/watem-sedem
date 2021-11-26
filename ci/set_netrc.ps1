50c6ac5b458008109c6fa7623e77606dfeaf8e9c
Johan Van de Wauw <johan@fluves.com>
2021-11-26 16:04:15 +0100

    Document netrc hack

--- ci/set_netrc.ps1
+++
@@ -1,7 +0,0 @@
-if ($Env:DRONE_NETRC_MACHINE) {
-@"
-machine $Env:DRONE_NETRC_MACHINE
-login $Env:DRONE_NETRC_USERNAME
-password $Env:DRONE_NETRC_PASSWORD
-"@ |Out-File -FilePath (Join-Path $Env:USERPROFILE '_netrc') -Encoding ascii;
-}
