b891e459b1cb420300e79bcd09467166fc43fb90
Johan Van de Wauw <johan@fluves.com>
2021-11-26 10:41:53 +0100

    FIx windows issues (#207)
    Reviewed-on: https://git.fluves.net/fluves/cn_ws/pulls/207
    Co-authored-by: Johan Van de Wauw <johan@fluves.com>
    Co-committed-by: Johan Van de Wauw <johan@fluves.com>

--- conda/bld.bat
+++ conda/bld.bat
@@ -2,4 +2,4 @@
 c:\lazarus\lazbuild.exe cn_ws\cn_ws.lpr
 :: strip debug symbols
 c:\lazarus\fpc\3.2.0\bin\x86_64-win64\strip.exe cn_ws\cn_ws.exe
-copy cn_ws\cn_ws.exe %LIBRARY_LIB%
+copy cn_ws\cn_ws.exe %LIBRARY_BIN%
