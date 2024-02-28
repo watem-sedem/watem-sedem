9a0cb4e6c6022894cc18ae913c6040b88456003e
Daan Renders <daan@fluves.com>
2024-02-28 16:15:53 +0100

    Update testscripts

--- testfiles/test.bat
+++ testfiles/test.bat
@@ -1,1 +1,1 @@
-.\cn_ws\cn_ws testfiles/molenbeek/modelinput/ini_molenbeek_scenario_1.ini
+.\watem_sedem\watem_sedem testfiles/molenbeek/modelinput/ini_molenbeek_scenario_1.ini
--- testfiles/test.sh
+++ testfiles/test.sh
@@ -1,5 +1,5 @@
 set -e
 rm -rf testfiles/molenbeek/modeloutput
 rm -rf testfiles/molenbeek/modeloutput_sdat
-cn_ws/cn_ws testfiles/molenbeek/modelinput_sdat/ini_molenbeek_scenario_1_sdat.ini
-cn_ws/cn_ws testfiles/molenbeek/modelinput/ini_molenbeek_scenario_1.ini
+watem_sedem/watem_sedem testfiles/molenbeek/modelinput_sdat/ini_molenbeek_scenario_1_sdat.ini
+watem_sedem/watem_sedem testfiles/molenbeek/modelinput/ini_molenbeek_scenario_1.ini
