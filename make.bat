c342e93217362023fc592c55c58cd96a35a1b169
Daan Renders <daan@fluves.com>
2024-02-28 16:06:08 +0100

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 for /f "tokens=*" %%A in ('git describe --abbrev^=4 --always --tags') do set var=%%A
 echo '%var%' > watem_sedem\version.inc
-c:\lazarus\lazbuild.exe watem_sedem\cn_ws.lpr
+c:\lazarus\lazbuild.exe watem_sedem\watem_sedem.lpr
 :: strip debug symbols
-c:\lazarus\fpc\3.2.0\bin\x86_64-win64\strip.exe cn_ws\cn_ws.exe
+c:\lazarus\fpc\3.2.0\bin\x86_64-win64\strip.exe watem_sedem\watem_sedem.exe
 echo 'DEVELOP' > watem_sedem\version.inc
