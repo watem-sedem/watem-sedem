6c6faf0de3b3846b4cbaa3bfaaeda943606262a2
Johan Van de Wauw <johan@fluves.com>
2024-05-13 17:01:10 +0200

    Build images for github packages

--- ci/build.sh
+++ ci/build.sh
@@ -1,2 +1,2 @@
-docker build . -t registry.fluves.net/drone/cn_ws/debian11-build
-docker build . -f Dockerfile-cnws_test -t registry.fluves.net/drone/cn_ws/cnws_test
+docker build . -t registry.fluves.net/drone/watem-sedem/debian11-build -t ghcr.io/watem-sedem/ci/debian11-build
+docker build . -f Dockerfile-test -t registry.fluves.net/drone/watem-sedem/watem-sedem_test ghcr.io/watem-sedem/ci/watem-sedem-test
