c4255abfadf575d260a5809b4fb6552dd13b30ca
Johan Van de Wauw <johan@fluves.com>
2024-05-13 17:22:20 +0200

    Also build conda image

--- ci/build.sh
+++ ci/build.sh
@@ -1,2 +1,2 @@
 docker build . -t registry.fluves.net/drone/watem-sedem/debian11-build -t ghcr.io/watem-sedem/ci/debian11-build
-docker build . -f Dockerfile-test -t registry.fluves.net/drone/watem-sedem/watem-sedem_test ghcr.io/watem-sedem/ci/watem-sedem-test
+docker build . -f Dockerfile-test -t registry.fluves.net/drone/watem-sedem/watem-sedem_test -t ghcr.io/watem-sedem/ci/watem-sedem-test
